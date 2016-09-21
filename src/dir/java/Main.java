1f7aaa85b027cda782429e40e910c73e43f82297
inAle <customerservice.ticketone@gmail.com>
2016-09-21 15:03:47 +0200

    progetto base

---
+++ src/dir/java/Main.java
@@ -0,0 +1,15 @@
+package dir.java;
+
+/**
+ * Classe di esempio per avvio di dw
+ * @author directa
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
